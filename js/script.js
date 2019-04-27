c74aac26caf960dd24fa45a2c4b5e267afb94ea4
Lucas Frota <lucasfvbo1@gmail.com>
2019-04-27 15:56:56 -0300

    subindo

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    
+    
+    /* Sticky nav */
+    $('.js--main').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+      offset: '150px;'
+    });
+    
+    
+    /* Scroll btn */
+    $('.js--scroll-to-club').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-club').offset().top}, 1000); 
+    });
+
+    /* Navigation scroll */
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html,body').animate({
+                scrollTop: target.offset().top
+              }, 1000);
+              return false;
+            }
+          }
+        });
+      });
+});
